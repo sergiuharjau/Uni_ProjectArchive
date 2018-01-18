49302d5f6452f636be411763149e4e9d22f19a49
Sergiu Harjau (harjaus) <harjaus@coventry.ac.uk>
2018-01-18 14:01:14 +0000

    Add files via upload

--- miner.py
+++ miner.py
@@ -1,17 +1,17 @@
 import subprocess, time
 
-def stopwatch(seconds):
+def stopwatch(minutes):
     start = time.time()
     time.clock()
     elapsed = 0
-    while elapsed < seconds:
+    while elapsed < minutes:
         elapsed = time.time() - start
-        time.sleep(1)
+        time.sleep(60)
 
 while True:
     print("Oh shit we up and running")
     p = subprocess.Popen("exec " + "./cpuminer -a lyra2zoin -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u Greeny47.TestVps -p TestVps", shell=True)
-    stopwatch(120)
+    stopwatch(40)
     print("We dead baby")
     p.kill()
-    stopwatch(120)
+    stopwatch(20)
