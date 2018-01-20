5e2386ff54585d08ce9297918cda30d150d64733
Sergiu Harjau (harjaus) <harjaus@coventry.ac.uk>
2018-01-20 02:01:21 +0000

    Add files via upload

---
+++ miner.py
@@ -0,0 +1,17 @@
+import subprocess, time
+
+def stopwatch(minutes):
+    start = time.time()
+    time.clock()
+    elapsed = 0
+    while elapsed < minutes:
+        elapsed += 1 
+        time.sleep(60)
+
+while True:
+    print("Oh shit we up and running")
+    p = subprocess.Popen("exec " + "./cpuminer -a lyra2zoin -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u Greeny47.Joyent1 -p Joyent1", shell=True)
+    stopwatch(40)
+    print("We dead baby")
+    p.kill()
+    stopwatch(20)
