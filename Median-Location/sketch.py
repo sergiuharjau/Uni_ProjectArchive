0644f2525b7aea2cd4090f8f2c716f35a4f51f06
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-05-22 12:41:22 +0000

    Now finds recursively looks for the closest city from Median Coordinates.

--- Median-Location/sketch.py
+++ Median-Location/sketch.py
@@ -25,9 +25,9 @@ def determineCities():
 
 def getCoordinates(city):
     
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address="
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="
     
-    city_url = url + city
+    city_url = url + city + "&key=AIzaSyDKGESVCnLJSfToUDSST5Ydx8ei9bf4ttM"
       
     data = urllib.request.urlopen(city_url)
    
@@ -35,7 +35,7 @@ def getCoordinates(city):
     
     if results == []:
         print("Google API prevented us from getting: " + city[:len(city)-1])
-        return({"lat" : 0 , "lng" : 0})                    
+        return(getCoordinates(city))                    
 
     coordinates = results[0]["geometry"]["location"]
 
@@ -55,7 +55,65 @@ def getMedian(allCoordinates):
     
     return({"lat" : medianLat , "lng" : medianLng})
     
+def findCityAt(coordinates, lookAT = -1, factor = 0.01):
     
+    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+    
+    latitude = coordinates["lat"] 
+    longitude = coordinates["lng"] 
+    ##print(lookAT)
+    if lookAT == -1:
+        pass
+    elif lookAT == 0: #North
+        latitude += factor #increments by 0.01 every time we go back to North 
+    elif lookAT == 1: #East
+        longitude += factor 
+    elif lookAT == 2: #South
+        latitude -= factor
+    elif lookAT == 3: #West
+        longitude -= factor   
+    
+    location_url = url + str(latitude) + "," + str(longitude) + "&key=AIzaSyDKGESVCnLJSfToUDSST5Ydx8ei9bf4ttM"
+    
+    data = urllib.request.urlopen(location_url)
+   
+    results = json.loads(data.read().decode())["results"]
+    
+    if results == []:
+        print("That may be in the ocean.")
+        print("Here's the coordinates though!")
+        print(str(round(coordinates["lat"],2)) + " , " + str(round(coordinates["lng"],2)))
+        return("Undefined", "Undefined")
+    
+    address_components = results[0]["address_components"]
+    
+    city = "Undefined"
+    
+    for i in range(len(address_components)):
+        if "locality" in address_components[i]["types"]:
+            city = address_components[i]["long_name"]
+        if "country" in address_components[i]["types"]:
+            country = address_components[i]["long_name"]
+    
+    if city == "Undefined":
+        lookAT += 1 
+        if lookAT > 3:
+            lookAT = 0
+            factor += 0.01
+        return ( findCityAt({"lat" : latitude , "lng" : longitude } , lookAT , factor ) )
+    
+    return ( city, country )  
+
+def findInDatabase(city, country, coordinates):
+    
+    f1 = open("/home/codio/workspace/worldcitiespop.txt", "r", encoding = "ISO-8859-1")
+	
+    bigList = f1.readlines() 
+
+    for line in bigList:
+        if city.lower() in line and country.lower() in line:
+            if str(round(coordinates["lat"],1)) in line and str(round(coordinates["lng"],1)) in line:
+                print(line)
     
 if __name__ == "__main__":
     
@@ -76,8 +134,13 @@ if __name__ == "__main__":
     googleURL = "https://www.google.co.uk/maps/place/"
     
     link = googleURL + str(median["lat"]) + "," + str(median["lng"]) + "/"
-    
+        
     print(link)
+    
+    city, country = findCityAt({"lat" : 44.308271, "lng" : 26.686003})
+               
+    print(city, country)
+
         
         
         
