3563a10ba01f60abcfc6955dec612bd2a057e3db
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-07-13 23:42:13 +0000

    Implemented Sheets database. Much simpler to use. Code housekeeping.

--- BudgetPlanner/bp.py
+++ BudgetPlanner/bp.py
@@ -1,3 +1,5 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
        
 class Budget:
     rent = 150 
@@ -8,14 +10,11 @@ class Budget:
     leisure = 0 
     
     def __init__(self):
-              
-        self.weekNumber = -1
         
-        self.loadFromDB() 
-     
-        self.necessities = float(input("\n\nNecessities spent: "))
-        self.holiday = float(input("Holiday spent: ")) 
-        self.leisure = float(input("Leisure spent: "))
+        self.initiateConnection() 
+        self.getPreviousFunds() 
+        self.getSheetsInputs() 
+      
         Budget.leisure -= self.leisure 
         Budget.holiday -= self.holiday
         
@@ -24,95 +23,76 @@ class Budget:
         
         self.holidayAdded = 0 
         self.leisureAdded = 0 
-        
-        self.work = int(input("Work money: "))
-        self.sideHustle = int(input("Hustle money: ")) 
+
         self.allowance = 20
 
+    def initiateConnection(self):
+        
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("Spreadsheet Updater-bdd3fd26a51b.json", scope)
+        gc = gspread.authorize(credentials)
+
+        self.wks = gc.open("Budget Planner").sheet1 
+        
+        self.weekRow = int(input("What week? ")) + 3  # Week 0 = Row 3 
+        self.weekNumber = self.weekRow - 3 
     
+    def getPreviousFunds(self):
+        
+        Budget.rentFund = float(self.wks.cell( self.weekRow, 4).value)
+        Budget.buffer   = float(self.wks.cell( self.weekRow, 5).value)
+        Budget.holiday  = float(self.wks.cell( self.weekRow, 6).value)
+        Budget.leisure  = float(self.wks.cell( self.weekRow, 7).value)
+    
+    def getSheetsInputs(self):
+        
+        # Inputs = I  J K / M  N 
+        #          9 10 11 13 14 
+        self.necessities = float(self.wks.cell( self.weekRow,  9).value)
+        self.holiday     = float(self.wks.cell( self.weekRow, 10).value)
+        self.leisure     = float(self.wks.cell( self.weekRow, 11).value)
+        self.work        = float(self.wks.cell( self.weekRow, 13).value)
+        self.sideHustle  = float(self.wks.cell( self.weekRow, 14).value)
+    
+    def writeToSheets(self):
+        
+        #Outputs = D E F G 
+        #          4 5 6 7 
+        self.wks.update_cell(self.weekRow + 1, 4, Budget.rentFund)
+        self.wks.update_cell(self.weekRow + 1, 5, Budget.buffer)
+        self.wks.update_cell(self.weekRow + 1, 6, Budget.holiday)
+        self.wks.update_cell(self.weekRow + 1, 7, Budget.leisure)
+        
     def moneyIN(self, money):
        
-        print("\nMoney in " + str(money))
+        #print("\nMoney in " + str(money))
     
         if money > Budget.rent - self.rentPaid:
             if self.rentPaid != Budget.rent: 
-                print("Paying rent: " + str(Budget.rent - self.rentPaid))
+                #print("Paying rent: " + str(Budget.rent - self.rentPaid))
                 money -= (Budget.rent - self.rentPaid)  
-                print("Money left: " + str(money))
+                #print("Money left: " + str(money))
                 self.rentPaid = Budget.rent 
                           
             if money > self.necessities - self.necPaid:
                 if self.necPaid != self.necessities:
-                    print("Paying necessities: " + str(self.necessities - self.necPaid))
+                    #print("Paying necessities: " + str(self.necessities - self.necPaid))
                     money -= (self.necessities - self.necPaid) 
-                    print("Money left: " + str(money))
+                    #print("Money left: " + str(money))
                     self.necPaid = self.necessities
-                print("Distributing: " + str(money))
+                #print("Distributing: " + str(money))
                 Budget.buffer += 0.5 * money 
                 self.holidayAdded += 0.2 * money 
                 self.leisureAdded += 0.3 * money 
                 
             else: 
-                print("Paying it all to necessities. " + str(money))                
+                #print("Paying it all to necessities. " + str(money))                
                 self.necPaid += money
                 
         else:
-            print("Paying it all to rent. " + str(money))
+            #print("Paying it all to rent. " + str(money))
             self.rentPaid += money 
-
-     
-    def overview(self):
-        print("\nWeek " + str(self.weekNumber))
-        print("\nNecessities spent: " + str(self.necessities))
-        print("Leisure spent: " + str(self.leisure))
-        print("\nWork money: " + str(self.work))
-        print("Holiday added: " + str(self.holidayAdded))
-        print("Leisure added: " + str(self.leisureAdded))
-        print("\nState of Rent Fund: " + str(Budget.rentFund))
-        print("State of Buffer: " + str(Budget.buffer))
-        print("State of Holiday: " + str(Budget.holiday))
-        print("State of Leisure: " + str(Budget.leisure))
-    
-    def indexToDB(self):
-        f1 = open("database.txt" , "w")
-        
-        values = [] 
-        
-        values.append(self.weekNumber)
-        values.append(Budget.rentFund)
-        values.append(Budget.buffer)
-        values.append(Budget.holiday)
-        values.append(Budget.leisure)
-        
-        print("\nThis is what we are indexing to the database.")
-        for element in values:
-            print(round(element,2), end ="; ")
-        print("")
-        for element in values:
-            f1.write(str(round(element,2)) + "\n")
-            
-        f1.close() 
-    
-    def loadFromDB(self):
-        f1 = open("database.txt" , "r")
-        rawValues = f1.readlines() 
-
-        values = [] 
-        
-        for element in rawValues:
-            values.append(element.replace('\n',''))
-         
-        print("\nThese are the values we have imported.")
-        print(values)
-        
-        self.weekNumber = float(values[0]) + 1 
-        Budget.rentFund = float(values[1])
-        Budget.buffer = float(values[2])
-        Budget.holiday = float(values[3])
-        Budget.leisure = float(values[4])
-        
-        f1.close() 
-        
+ 
     def finishWeek(self):
         
         self.moneyIN(self.work)
@@ -120,14 +100,14 @@ class Budget:
         self.moneyIN(self.allowance)
 
         if self.rentPaid < Budget.rent: 
-            print("\nMissing " + str(Budget.rent - self.rentPaid) + " of rent")
-            print("Taking it from Buffer.")
+            #print("\nMissing " + str(Budget.rent - self.rentPaid) + " of rent")
+            #print("Taking it from Buffer.")
             payIN = Budget.rent - self.rentPaid
             self.moneyIN(payIN)
             Budget.buffer -= payIN 
         if self.necPaid < self.necessities:
-            print("\nMissing " + str(self.necessities - self.necPaid) + " of necessities")
-            print("Taking it from Buffer.")
+            #print("\nMissing " + str(self.necessities - self.necPaid) + " of necessities")
+            #print("Taking it from Buffer.")
             payIN = self.necessities - self.necPaid
             self.moneyIN(payIN)
             Budget.buffer -= payIN
@@ -143,10 +123,10 @@ class Budget:
         Budget.leisure += self.leisureAdded
         
         Budget.rentFund += self.rentPaid 
+
+        self.writeToSheets() 
         
-        self.overview() 
-        
-        self.indexToDB() 
+        print("All good!")
         
 if __name__ == "__main__":
     week = Budget()
