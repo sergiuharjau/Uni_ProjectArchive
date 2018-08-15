9ffffbd2f9299e83025ab1f41045a14e6e6db0dc
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-08-15 17:01:20 +0000

    Added turns. Only Pawns can move

--- ChessJS/chess.js
+++ ChessJS/chess.js
@@ -4,7 +4,10 @@ var currentState = [] ;
 var currentState2D = [ 
                 [],[],[],[],[],[],[],[] 
                             ] ; 
-var increment = 0; 
+var increment = 0;
+var piece;
+var turn; 
+var availableBoxes = [];
 
 function setup(){
     createCanvas(8 * scl, 8 * scl );
@@ -12,6 +15,9 @@ function setup(){
     drawBoard() ; 
     setupPieces() ;
     drawPieces() ; 
+    
+    console.log("White plays")
+    turn = "white"
 }
 
 function draw(){
@@ -19,7 +25,7 @@ function draw(){
 }
 
 function setupPieces(){
-    currentState = [3, 4, 5, 2, 1, 5, 4, 3, 6, 6, 6, 6, 6, 6, 6, 6] ; 
+    currentState = [-3, -4, -5, -2, -1, -5, -4, -3, -6, -6, -6, -6, -6, -6, -6, -6] ; 
     
     for(i = 0 ; i < 32 ; i ++){
         append(currentState, 0)
@@ -82,20 +88,34 @@ function drawPieces(){
     }
 }
 
-function mousePressed(){  
-    selectPiece() ; 
+function advanceTurn(){
+    if (turn == "white"){
+        turn = "black"
+    } else if (turn == "black"){
+        turn = "white"
+    }
+}
+
+function mousePressed(){
+    boxPos = currentBox() 
+    for (i = 0 ; i < availableBoxes.length ; i ++) {
+        if (boxPos[0] == availableBoxes[i][0] && boxPos[1] == availableBoxes[i][1]){
+            advanceTurn() ; 
+            console.log(turn, "plays") ;
+        }
+    } 
+    selectPiece(turn) ; 
 }
 
 function currentBox(){
     return [floor(mouseX / scl), floor(mouseY/scl)]
 }
 
-function selectPiece(){
+function selectPiece(turn){
     drawBoard() ; //to override past selects
     drawPieces() ;    
     
     boxPos = currentBox() ;
-
     if (currentState2D[boxPos[1]][boxPos[0]] != 0){
         //if no piece in square, can't select  
         if ((boxPos[0] + boxPos[1]) % 2 == 0){
@@ -103,6 +123,36 @@ function selectPiece(){
         }else{
             fill(0,0,255,80);
         }
-        rect(boxPos[0] * scl , boxPos[1] * scl , scl-1, scl-1)  
+        
+        if (turn == "white"){
+            if (currentState2D[boxPos[1]][boxPos[0]] > 0){
+                rect(boxPos[0] * scl , boxPos[1] * scl , scl-1, scl-1)  
+                giveOption(currentState2D[boxPos[1]][boxPos[0]], boxPos, "white") ;
+            }
+        }else if (turn == "black"){
+            if (currentState2D[boxPos[1]][boxPos[0]] < 0){
+                rect(boxPos[0] * scl , boxPos[1] * scl , scl-1, scl-1)  
+                giveOption(currentState2D[boxPos[1]][boxPos[0]], boxPos, "black") ;   
+            }
+        }
+    }
+
+}
+
+function giveOption(piece, boxPos, colour){
+    if (piece == 6 || piece == -6){
+        if (colour == "white"){
+            fill(0,255,0)
+            rect(boxPos[0] * scl, (boxPos[1]-1) * scl, scl -1, scl -1)
+            rect(boxPos[0] * scl, (boxPos[1]-2) * scl, scl -1, scl -1)
+            availableBoxes = [ [boxPos[0], boxPos[1]-1] , [boxPos[0], boxPos[1]-2] ]
+            console.log(availableBoxes)
+        }else{
+            fill(0,255,0)
+            rect(boxPos[0] * scl, (boxPos[1]+1) * scl, scl -1, scl -1)
+            rect(boxPos[0] * scl, (boxPos[1]+2) * scl, scl -1, scl -1)
+            availableBoxes = [ [boxPos[0], boxPos[1]+1 ] , [boxPos[0], boxPos[1]+2] ]
+            console.log(availableBoxes)
+        }
     }
 }
