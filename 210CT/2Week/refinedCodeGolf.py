251bbaf4b7ec0c9b37117c982950c13aafd787ef
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-10-15 13:50:14 +0000

    627 bytes, 25 lines

--- 210CT/2Week/refinedCodeGolf.py
+++ 210CT/2Week/refinedCodeGolf.py
@@ -1,4 +1,4 @@
-z=range;o=len
+z=range;o=len;p=print
 def s(a):
  r=[[1]*5 for y in z(5)];e=[] 
  for x in z(o(a)):
@@ -15,16 +15,11 @@ def s(a):
        except:pass
    if a[x][y]=="F":r[x][y]="H" 
  return r
-a=[[1]*5 for y in z(5)];j=0
-for l in open("i","r").readlines():  
- i=0 
- for e in l:
-  if e!=" ":
-   if i==5:break
-   a[j][i]=e;i+=1 
- j+=1 
+a=[[1]*5 for y in z(5)]
+for j in z(5):
+ for i in z(5):a[j][i]=open("i","r").readlines()[j][i]
 g=int(input())
 for i in z(g):a=s(a)
 for i in z(o(a)):
- for j in z(o(a)):print(a[i][j],end=" ")
- print()
+ for j in z(o(a)):p(a[i][j],end="")
+ p()
