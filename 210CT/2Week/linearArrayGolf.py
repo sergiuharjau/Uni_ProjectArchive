62697dc2ffd668aae86d02ea08aa9a76b567b6f8
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-10-16 14:06:48 +0000

    425 bytes. Need perfect input file. 5x5 character set, no white spaces anywhere.

---
+++ 210CT/2Week/linearArrayGolf.py
@@ -0,0 +1,19 @@
+def s(a):
+ r=[1]*25;e=[]
+ for i in z(25):
+  if i in e:k=0
+  elif a[i]=="I":
+   r[i]="F"
+   for x in [-5,-1,1,5]:
+    try:
+     if i%5 in (0,4) and x in (-1,1):k=0 
+     elif a[x+i]=="H":r[x+i]="I";e.append(x+i)
+    except:k=0
+  else:r[i]="H"   
+ return r 
+z=range;a=[]
+for i in z(25):a.append(open("i","r").read().replace('\n','')[i])
+for i in z(int(input())):a=s(a)
+for i in z(25):
+ print(a[i],end="")
+ if (i+1)%5==0:print()
