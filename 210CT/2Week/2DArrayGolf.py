b6bc55452a31a10da455d348254b599b8e3f1f8a
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-10-16 14:17:13 +0000

    Removed whitespace. 537 bytes.

--- 210CT/2Week/2DArrayGolf.py
+++ 210CT/2Week/2DArrayGolf.py
@@ -1,13 +1,13 @@
 def s(a):
- r=[[1]*5 for y in z(5)];e=[] 
+ r=[[1]*5 for y in z(5)];e=[]
  for x in z(5):
-  for y in z(5):    
-   if (x,y) in e:j=0                     
+  for y in z(5):
+   if (x,y) in e:j=0                 
    elif a[x][y]=="I":
     r[x][y]="F"
     for m in z(-1,2):
      for k in z(-1,2):
-      if m!=k and m!=-k: 
+      if m!=k and m!=-k:
        try:
         if a[x+m][y+k]=="H":r[x+m][y+k]="I";e.append((x+m,y+k))
        except:j=0
