f3d7b886d6eb5f5dd948f4f0dffd772ad7da5337
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-10-30 13:29:29 +0000

    Changed True->1 and False->0

--- 210CT/4Week/refined.py
+++ 210CT/4Week/refined.py
@@ -1,7 +1,7 @@
 def p(g):
  for i in z(l(g)//2):
-  if g[i]!=g[l(g)-i-1]:return False
- return True
+  if g[i]!=g[l(g)-i-1]:return 0
+ return 1
 t=input();m="";s=[];l=len;z=range
 for i in z(l(t)):
  for j in z(i+1,l(t)):s.append(t[i:j+1])
