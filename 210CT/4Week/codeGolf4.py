e7a473345a2c2feb7bc627ed3c5981eda3d6896c
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-11-02 11:12:32 +0000

    Changed filenames

---
+++ 210CT/4Week/codeGolf4.py
@@ -0,0 +1,11 @@
+def p(g):
+ for i in z(l(g)//2):
+  if g[i]!=g[l(g)-i-1]:return 0
+ return 1
+t=input();m="";s=[];l=len;z=range
+for i in z(l(t)):
+ for j in z(i+1,l(t)):s.append(t[i:j+1])
+for e in s:
+ if p(e):
+  if l(e)>l(m):m=e
+print(m)
