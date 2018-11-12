5f422dfb9ebfd97c718e5b54e318054673395c22
Sergiu Harjau <harjaus@uni.coventry.ac.uk>
2018-11-12 16:59:47 +0000

    Relevant tests for depthFirst()

--- 210CT/Assignment/test_Graph.py
+++ 210CT/Assignment/test_Graph.py
@@ -6,12 +6,13 @@ class TestGraph(unittest.TestCase):
     def setUp(self):
         """Initiates a graph"""
         self.simpleG = gs.GraphStructure([1, 2, 3, 4, 5], [(1,2), (2,4), (3,4), (3,5), (4,5)])
-        self.complexG = gs.GraphStructure([6, 7, 8, 9, 10, 11, 12, 13, 21, 24], [(6,7), (7,21), (21,24), (6,9), (6,8), (8,12), (12,9), (9,11), (9,10), (12,13)])
-    
+        self.complexG = gs.GraphStructure([6, 7, 8, 9, 10, 11, 12, 13, 21, 24], [(6,7), (7,21), (21,24), (6,9), (6,8), (8,12), (12,9), (9,11), (9,10), (12,13)])   
+        self.brokenGraph = gs.GraphStructure([1,2,3,4], [(1,2), (2,3)])
+        
     def test_Creation(self):
         """Sanity check."""
-        self.assertEqual(self.simpleG.getAdjacency(), {1: [2], 2: [1, 4], 3: [4, 5], 4: [2, 3, 5], 5: [3, 4]})
-        self.assertEqual(self.complexG.getAdjacency(), {6: [7,9,8], 7: [6, 21], 8: [6,12], 9:[6, 12, 11, 10], 10: [9], 11: [9], 12: [8, 9, 13], 13:[12], 21:[7, 24], 24:[21]})
+        self.assertEqual(self.simpleG.adjacency, {1: [2], 2: [1, 4], 3: [4, 5], 4: [2, 3, 5], 5: [3, 4]})
+        self.assertEqual(self.complexG.adjacency, {6: [7,9,8], 7: [6, 21], 8: [6,12], 9:[6, 12, 11, 10], 10: [9], 11: [9], 12: [8, 9, 13], 13:[12], 21:[7, 24], 24:[21]})
     
     def test_isPath(self):
         """Checks isPath() behaviour"""
@@ -38,10 +39,25 @@ class TestGraph(unittest.TestCase):
         
         self.assertEqual(self.simpleG.isConnected(), True)
         self.assertEqual(self.complexG.isConnected(), True)
-        
-        self.brokenGraph = gs.GraphStructure([1,2,3,4], [(1,2), (2,3)])
         self.assertEqual(self.brokenGraph.isConnected(), False)
         
+    def test_breadthFirst(self):
+        """Checks traverseBreadthFirst() behaviour."""
         
+        self.assertEqual(self.simpleG.traverseBreadthFirst(4), [4, 2, 3, 5, 1])
+        self.assertEqual(self.complexG.traverseBreadthFirst(9), [9, 6, 12, 11, 10, 7, 8, 13, 21, 24])
+        self.assertEqual(self.brokenGraph.traverseBreadthFirst(2), [2,1,3,4]) 
+        self.assertEqual(self.brokenGraph.traverseBreadthFirst(4), [4,1,2,3])
+    
+    def test_depthFirst(self):
+        """Checks traverseDepthFirst() behaviour."""
+        
+        self.assertEqual(self.complexG.traverseDepthFirst(9), [9, 6, 7, 21, 24, 8, 12, 13, 11, 10])
+        self.assertEqual(self.complexG.traverseDepthFirst(11), [11, 9, 6, 7, 21, 24, 8, 12, 13, 10]) 
+        self.assertEqual(self.complexG.traverseDepthFirst(8), [8, 6, 7, 21, 24, 9, 12, 13, 11, 10])
+            #can only be really seen on big graphs
+    
+    
+    
 if __name__ == "__main__":
     unittest.main() 
