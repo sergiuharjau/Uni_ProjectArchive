4581bc1ff3788ee78f21f2736fd362f8f72e42fb
harjaus <harjaus@coventry.ac.uk>
2020-02-18 14:59:46 -0800

    Added orange cone support. Latest map

--- FreeTime/Canvassing/createMap.py
+++ FreeTime/Canvassing/createMap.py
@@ -1,3 +1,4 @@
+import os
 
 def getInput():
     x = []
@@ -5,8 +6,8 @@ def getInput():
     label = []
     for line in open("mapInput.txt").readlines():
         values = line[:-1].split(",")
-        y.append(values[0]) #webots is reversed
-        x.append(values[1])
+        y.append(str(float(values[0]))) #webots is reversed
+        x.append(str(float(values[1])-50))
         label.append(values[2])
 
     return x, y, label
@@ -21,8 +22,11 @@ if __name__ == "__main__":
     for x, y, label in zip(xList, yList, labelList):
         if label == " 3": 
             colour = "blue"
-        else:
+        elif label == " 8":
             colour = "yellow"
+        elif label == " 5":
+        	colour = "orange"
+           
         translation = str(round(float(x),2)) + " -0.3 " + str(round(float(y),2))
         name = "cone" + colour + str(i)
         content = f'\n\ttranslation {translation}\n\tname "{name}"\n\tcolor "{colour}"\n'
@@ -30,4 +34,7 @@ if __name__ == "__main__":
         i+=1
         result += fullString 
 
-    open("result.txt", "w").write(result)
+
+    open("new.wbt", "w").write(open("template.wbt", "r").read() + result)
+    os.system("cp new.wbt ~/Downloads/rc_webots/worlds/.")
+    os.system("echo Done!")
